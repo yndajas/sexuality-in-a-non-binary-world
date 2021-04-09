3c6c1673822b6c8b1c0679e95cd9d52373d2491a
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-04-10 00:31:13 +0100

    add arrow key and space bar navigation

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,26 @@
-document.addEventListener('keydown', e => {
-    console.log(e.code);
+document.addEventListener('DOMContentLoaded', () => {
+    const previousButton = document.getElementById('previous');
+    let previousUrl = null;
+    if (previousButton) {
+        previousUrl = previousButton.href;
+    }
+    
+    const nextButton = document.getElementById('next');
+    let nextUrl = null;
+    if (nextButton) {
+        nextUrl = nextButton.href;
+    }
+
+    document.addEventListener('keydown', e => {
+        const key = e.code;
+
+        console.log(e.code);
+
+        if (key === 'ArrowLeft' && previousUrl !== null) {
+            window.location.href = previousUrl;
+        } else if (['ArrowRight', 'Space'].includes(key) && nextUrl !== null) {
+            window.location.href = nextUrl;
+        }
+    })    
 })
+
